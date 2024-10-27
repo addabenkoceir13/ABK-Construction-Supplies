175a4d88e84ca07c43ce6350305aa3ee610cfa77
addabenkoceir13 <addamohamed67@gmail.com>
2024-10-27 21:02:43 +0100

    update 27-10-2024

--- resources/views/content/dashboard/index.blade.php
+++ resources/views/content/dashboard/index.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts/contentNavbarLayout')
 
-@section('title', 'Dashboard - Analytics')
+@section('title', __('Dashboard - Analytics'))
 
 @section('vendor-style')
 <link rel="stylesheet" href="{{asset('assets/vendor/libs/apex-charts/apex-charts.css')}}">
